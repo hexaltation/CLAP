9a7442f46c5f0fd5607e2a14fbe33e920d307955
Grégoire Cutzach <caztuc@hotmail.com>
2018-09-23 19:47:37 +0200

    ADDED Color Boxes Drawing

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let containers = document.getElementsByClassName('colorLevel');
-let canvas_default = {
+let canvas_default ={
                         "width":120,
                         "height":120,
-                        "background":"#000000"
-}
+                        "background":"#000000",
+                        "margin":10
+                    }
 let colorLevels =   { 
                         "red":  {
                                 "in":{
@@ -55,7 +56,30 @@ function draw(elem){
         ctx.canvas.width = canvas_default.width;
         ctx.canvas.height = canvas_default.height;
         ctx.canvas.style.background = canvas_default.background;
+        draw_levels(ctx);
     }catch(e){
         console.log(e);
     }
+}
+
+function draw_levels(ctx){
+    for (let color in colorLevels){
+        draw_level(ctx, colorLevels[color]);
+    }
+}
+
+function draw_level(ctx, color){
+    console.log((color.in.min*(canvas_default.width-2*canvas_default.margin))/255,(color.in.max*(canvas_default.width-2*canvas_default.margin))/255, (color.out.max*(canvas_default.width-2*canvas_default.margin))/255, (color.out.min*(canvas_default.width-2*canvas_default.margin))/255)
+    ctx.fillStyle=color.color_value;
+    ctx.beginPath();
+    ctx.moveTo((color.in.min*(canvas_default.width-2*canvas_default.margin))/255 + canvas_default.margin,
+                0 + canvas_default.margin);
+    ctx.lineTo((color.in.max*(canvas_default.width-2*canvas_default.margin))/255 + canvas_default.margin,
+                0 + canvas_default.margin);
+    ctx.lineTo((color.out.max*(canvas_default.width-2*canvas_default.margin))/255 + canvas_default.margin,
+                canvas_default.height - canvas_default.margin);
+    ctx.lineTo((color.out.min*(canvas_default.width-2*canvas_default.margin))/255 + canvas_default.margin,
+                canvas_default.height - canvas_default.margin);
+    ctx.closePath();
+    ctx.fill();
 }
