19d0d9b1adca62f7d6d86d9890aac1d190c5389a
Grégoire Cutzach <caztuc@hotmail.com>
2018-10-12 09:50:01 +0200

    ADDED gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'build/<%= pkg.name %>.es2015.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+}
